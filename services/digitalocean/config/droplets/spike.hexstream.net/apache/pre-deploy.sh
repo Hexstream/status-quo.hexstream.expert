5827e346e225a48b2e514a9c2945baf76a771482
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2016-11-17 23:35:14 -0500

    Update apache config and infrastructure. Upload OpenSSH client config in new "raw" section.

--- services/digitalocean/config/droplets/spike.hexstream.net/apache/pre-deploy.sh
+++ services/digitalocean/config/droplets/spike.hexstream.net/apache/pre-deploy.sh
@@ -2,11 +2,19 @@
 
 DIR=$PRE_DEPLOY_DIR/apache
 
+echo Pre-deploying to $DIR...
+
 rm -rf $DIR
 mkdir $DIR
 
-cp -rT /home/status-quo.hexstreamsoft.com/public/services/digitalocean/config/droplets/spike.hexstream.net/apache/ $DIR
+mkdir $DIR/public/
+cp -rT /home/status-quo.hexstreamsoft.com/public/services/digitalocean/config/droplets/spike.hexstream.net/apache/ $DIR/public/
 
-rm $DIR/*.sh
+mkdir $DIR/private/
+cp -rT /home/private.status-quo.hexstream.xyz/public/apache/ $DIR/private/
 
-sed -i "s/\$HOME_IP/$(dig +short hq.hexstream.xyz)/" $DIR/site-wide-tunnels.conf
+rm $DIR/public/*.sh
+
+sed -i "s/\$HOME_IP/$(dig +short hq.hexstream.xyz)/" $DIR/public/site-wide-tunnels.conf
+
+echo Done.
