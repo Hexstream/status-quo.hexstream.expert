bef325392758b5257ccc469144d92f03434c6858
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-07-04 23:04:26 -0400

    Remove meta-nav, for now.

--- services/digitalocean/config/droplets/spike.hexstream.net/raw/website-update-hook.sh
+++ services/digitalocean/config/droplets/spike.hexstream.net/raw/website-update-hook.sh
@@ -9,10 +9,6 @@ cd ~/website.git/
 
 GIT_WORK_TREE=$PUBLIC git checkout -f
 
-# Remove canonical self-links in production
-sed -i -e '/<nav id="meta-nav"/{n;N;N;s/.*[Cc]anonical.*/<delete>/;
-}; /<delete>/d' $(find $PUBLIC -name '*.html')
-
 aws s3 sync $PUBLIC s3://$WEBSITE/ --delete --cache-control "$CACHE_CONTROL"
 
 
