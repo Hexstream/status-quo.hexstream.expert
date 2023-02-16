06d37065be577cdde18a5365f4df1c335ce31662
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2023-02-16 11:25:18 -0500

    tmux: Use better window-status-format.

--- assets/tools/tmux/config/broadcast.sh
+++ assets/tools/tmux/config/broadcast.sh
@@ -6,7 +6,6 @@ windows=$(tmux list-windows -F '#{window_id}' -f "#{!=:#{@nobroadcast},1}")
 
 if [ -n "$windows" ]; then
     for sibling in $windows; do
-        echo "send -t '$sibling' $retval" >> /home/hexstream/data/projects/debug.txt
         tmux run -C "send -t '$sibling' $retval"
     done
 fi
